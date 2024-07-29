0410ba55ea7646b3cebf10b05cb7b63c71950938
Bhuval-Singh <ayushsinghbca2022@gmail.com>
2024-07-29 19:59:24 +0530

    Some change

--- LinkedListAddFirst.java
+++ LinkedListAddFirst.java
@@ -31,6 +31,10 @@ public void addLast(int data){
 
 }
 public void add(int idx,int data){
+    if(idx == 0){
+        addFirst(data);
+        return;
+    }
     Node newNode=new Node(data);
     Node temp=head;
     int i=0;
